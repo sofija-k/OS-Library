7187c2abead198ed6ed0b9afb6e14a8f8f7bdb3c
sofija-k <sofijakotrc@gmail.com>
2024-09-28 23:22:23 -0400

    Update thread.c

--- A1-Cooperative-Threads/thread.c
+++ A1-Cooperative-Threads/thread.c
@@ -4,17 +4,15 @@
 #include <stdio.h>
 #include "thread.h"
 
-* This is the wait queue structure, needed for Assignment 2. */ 
-struct waiting_thread{
-	Tid data;
-	struct waiting_thread* next;
-};
+// 1. a function for removing thread from front of queue
+/* 2. update yield() function, return -1 if yield impossible i.e
+		- queue is empty, you cannot yeild to another thread
+		- the thread with want_tid doesn't exist in queue
+ */ 
 
+/* This is the wait queue structure, needed for Assignment 2. */ 
 struct wait_queue {
-	struct waiting_thread* head;
-	struct waiting_thread* tail;
-	int size;
-	int id;
+	/* ... Fill this in Assignment 2 ... */
 };
 
 /* For Assignment 1, you will need a queue structure to keep track of the 
@@ -24,32 +22,27 @@ struct wait_queue {
  */
 
 /* This is the thread control block. */
+/* we need: rip, rsp, rbp, rdi, rsi*/
+/* we need: tid, thread_state("ready"=0 or "running"=1)*/
 struct thread {
 	ucontext_t context;
 	Tid Tid;
 	int* stack_addr;
-	bool killed;
-	bool sleeping;
-	struct wait_queue* wq;
-	Tid waiting_on;
-	int exit;
-	Tid parent;
-	bool stack_freed;
+	int killed;
 };
 
 Tid running_thread;
 Tid ready_queue[THREAD_MAX_THREADS];
 Tid available_threads[THREAD_MAX_THREADS];
 struct thread* created_threads[THREAD_MAX_THREADS];
-struct wait_queue* all_wait_queues[THREAD_MAX_THREADS*THREAD_MAX_THREADS];
 
 
 int num_threads_created = 0;
-bool returning_from_exit = false;
-int* zombie_stack_addr = NULL;
-Tid zombie_tid = (Tid) -300;
-int num_wait_queues = 0;
 
+int* zombie_stack_addr = NULL;
+int returning_from_exit = 0;
+Tid yield_to;
+Tid zombie_tid = (Tid) -300;
 /**************************************************************************
  * Assignment 1: Refer to thread.h for the detailed descriptions of the six
  *               functions you need to implement. 
@@ -58,24 +51,16 @@ int num_wait_queues = 0;
 void
 thread_init(void)
 {
-	
 	/* Add necessary initialization for your threads library here. */
         /* Initialize the thread control block for the first thread */
 	/* 1. initialize thread_control_block*/
 	/*what should stackPointer be for this thread? Null?*/
-	struct thread* init_thread = malloc369(sizeof(struct thread));
-
+	struct thread* init_thread = malloc(sizeof(struct thread));
+    
 	getcontext(&(init_thread->context));
-
     init_thread->Tid = 0;
-	init_thread->killed = false;
+	init_thread->killed = 0;
 	++ num_threads_created;
-	init_thread -> sleeping = false;
-	init_thread->wq = NULL;
-	init_thread -> waiting_on = -300;
-	init_thread -> exit = -300;
-	init_thread->parent = 0;
-	init_thread->stack_freed = false;
 
 	/* 2. initialize the ready_queue*/
 	running_thread = (Tid) 0;
@@ -98,26 +83,15 @@ Tid
 thread_id()
 {
 	return running_thread;
+	// return THREAD_INVALID;
 }
 
 /* New thread starts by calling thread_stub. The arguments to thread_stub are
  * the thread_main() function, and one argument to the thread_main() function. 
  */
-
-void
-freeup_leftover_zombies();
-
-void 
-thread_cleanup_zombie(bool freed_stack);
-
-void
-thread_routine_cleanup();
-
 void
 thread_stub(void (*thread_main)(void *), void *arg)
 {
-		thread_routine_cleanup();
-		interrupts_on();
         thread_main(arg); // call thread_main() function with arg
         thread_exit(0);
 }
@@ -139,27 +113,30 @@ void add_to_queue_tail(Tid id){
     }
 }
 
+// you will dynamycally allocate space for the stack on the thread struct
+// each thread should have at least THREAD_MIN_STACK bytes
+/* when we create our thread struct, we will call getcontext() to create a valid context for it, and then before we put this thread on the queues
+   we will adjust some registers in the thread to ensure it begins executing in the right place
+		- rpc should point to a stub function (this is the first function the newly created thread runs)
+		- there are two regesters for passed arguments, you need to update these registers to hold arguments passed to the stub function
+		- you need to allocate a new per-thread stack using malloc(), and have the rsp register hold the address of that stack on the heap
+		  it should point to the TOP of the new stack because the stack grows down*/
 Tid
 thread_create(void (*fn) (void *), void *parg)
 {
-	int e = interrupts_off();
 	// if no more space for another thread -> THREAD_NO_MORE
 	// if created_threads[THREAD_MAX_THREADS -1] != -1
-	if(num_threads_created == THREAD_MAX_THREADS){
-		interrupts_set(e);
-		return THREAD_NOMORE;}
+	if(num_threads_created == THREAD_MAX_THREADS){return THREAD_NOMORE;}
 	++num_threads_created;
 
 	// turns out we do have space to create a thread
-	struct thread* create_thread = malloc369(sizeof(struct thread));
-
-	assert (!interrupts_enabled());
+	struct thread* create_thread = malloc(sizeof(struct thread));
 	getcontext(&(create_thread->context));
-	
+
 	// what is the tid of our thread?
 	Tid create_thread_tid = available_threads[0];
-	remove_from_available();
 	create_thread->Tid = create_thread_tid;
+	remove_from_available();
 	/* now we have to adjust some values in our context before*/
 	// 1. rip has to point to stub function
 	create_thread->context.uc_mcontext.gregs[REG_RIP] = (unsigned long) &thread_stub;
@@ -167,18 +144,10 @@ thread_create(void (*fn) (void *), void *parg)
 	create_thread->context.uc_mcontext.gregs[REG_RDI]= (unsigned long) fn;
 	create_thread->context.uc_mcontext.gregs[REG_RSI]= (unsigned long) parg;
 	// 3. need to allocate a stack using malloc
-	int *lower_limit = malloc369(THREAD_MIN_STACK);
-	if (lower_limit == NULL) {
-		interrupts_set(e);
-		return THREAD_NOMEMORY;}
+	int *lower_limit = malloc(THREAD_MIN_STACK);
+	if (lower_limit == NULL) {return THREAD_NOMEMORY;}
 	create_thread->stack_addr = lower_limit;
-	create_thread->killed = false;
-	create_thread -> sleeping = false;
-	create_thread->wq = NULL;
-	create_thread -> waiting_on = -300;
-	create_thread -> exit = -300;
-	create_thread ->parent = running_thread;
-	create_thread->stack_freed = false;
+	create_thread->killed = 0;
 	// align (unsigned long)lower_limit) + (unsigned long) (THREAD_MIN_STACK) first 
 	unsigned long upper_limit = ((unsigned long)lower_limit) + (unsigned long) (THREAD_MIN_STACK) - (unsigned long) 8;
 	// 4. change the saved stack pointer register in the context to point to the top of the new stack
@@ -189,87 +158,14 @@ thread_create(void (*fn) (void *), void *parg)
 	// add this thread to ready_queue
 	add_to_queue_tail(create_thread_tid);
 	// return tid of created thread
-	interrupts_set(e);
+	if (create_thread_tid < 0){
+		printf("CREATED_ID NEGATIVE: %d\n ITERATION: %d \n", create_thread_tid, num_threads_created);
+	}
 	return create_thread_tid;
 
 }
 
-void 
-append_to_available(Tid val){
-	for (int i=0; i<THREAD_MAX_THREADS; ++i){
-		if (available_threads[i] == (Tid)-300){
-			available_threads[i] = val;
-			break;
-		}
-	}
-}
-
-void 
-thread_create_zombie(Tid thread){
-    zombie_tid = thread;
-    zombie_stack_addr = created_threads[(int)thread]->stack_addr;
-}
-
-void 
-cleanup_before_zombifying(Tid zombie){
-	/* CLEANUP WAITS BEFORE EXITING*/
-	struct thread* zombie_thread = created_threads[(int)zombie];
-	if (zombie_thread ->waiting_on != -300){
-		Tid waiting_for = zombie_thread ->waiting_on;
-		created_threads[waiting_for] -> wq -> head = NULL;
-		created_threads[waiting_for] -> wq -> tail = NULL;
-		created_threads[waiting_for] -> wq -> size = 0;
-	}
-	if (zombie_thread ->wq != NULL && zombie_thread->wq->head != NULL){
-		thread_wakeup(zombie_thread->wq, false);
-	}
-}
-
-void 
-thread_cleanup_zombie(bool freed_stack){
-	append_to_available(zombie_tid);
-    assert(zombie_tid != (Tid)-300);
-	wait_queue_destroy(created_threads[(int) zombie_tid]->wq);
-	created_threads[(int) zombie_tid]->wq = NULL;
-	assert(created_threads[(int) zombie_tid]->wq == NULL);
-
-	if (!freed_stack){
-		free369(created_threads[(int)zombie_tid]->stack_addr);
-		created_threads[(int)zombie_tid]->stack_addr = NULL;
-	}
-	assert (created_threads[(int)zombie_tid]->stack_addr == NULL);
-
-    free369(created_threads[(int) zombie_tid]);
-    created_threads[(int) zombie_tid] = NULL;
-    zombie_tid = (Tid)-300;
-
-    if (zombie_stack_addr != NULL){
-		free369(zombie_stack_addr);
-    	zombie_stack_addr = NULL;
-	}
-	
-}
-
-
-void
-thread_routine_cleanup(){
-	// check if running thread is exited
-	if (zombie_tid != -300){ 
-		//cleanup stack pointer
-		free369(created_threads[(int)zombie_tid]->stack_addr);
-		created_threads[(int)zombie_tid]->stack_addr = NULL;
-		created_threads[(int)zombie_tid]->stack_freed = true;
-		zombie_tid = (Tid)-300;
-		zombie_stack_addr = NULL;
-	}
-	interrupts_off();
-    if (created_threads[(int) running_thread]->killed){
-        thread_exit(-SIGKILL);
-    }
-}
-
-Tid 
-find_index_in_queue(Tid want_tid){
+Tid find_index_in_queue(Tid want_tid){
     for (int i = 0; i < THREAD_MAX_THREADS; ++i) {
         if (ready_queue[i] == want_tid) {
             return i; // Return the index if the target is found
@@ -278,16 +174,123 @@ find_index_in_queue(Tid want_tid){
     return -1; 
 }
 
-void 
-remove_from_queue(int index) {
+void remove_from_queue(int index) {
+    // Shift elements to the left
 
     for (int i = index; i < THREAD_MAX_THREADS -1; ++i) {
         ready_queue[i] = ready_queue[i + 1];
     }
 	ready_queue[THREAD_MAX_THREADS-1] = (Tid) -300;
     if (index < 0 || index >= THREAD_MAX_THREADS) {
+        printf("Invalid index\n");
         return;
     }
+
+}
+
+void append_to_available(Tid val){
+	for (int i=0; i<THREAD_MAX_THREADS; ++i){
+		if (available_threads[i] == (Tid)-300){
+			available_threads[i] = val;
+			break;
+		}
+	}
+}
+
+Tid
+thread_yield(Tid want_tid)
+{
+	/* store and restore the caller's state */
+	/* save thread's context in it's struct*/
+	/* take running_thread Tid and find the thread in created_threads list*/
+	
+    struct thread* curr_thread = created_threads[(int) running_thread];
+	struct thread* new_thread;
+	Tid new_thread_id;
+
+    /* get context of that running_thread*/
+	//get thread you need and set that context
+	if (want_tid == THREAD_SELF){
+		 return running_thread;
+	} else if (want_tid == THREAD_ANY){
+		
+		// take first thread from ready queue
+        // if queue is empty return THREAD_NONE
+        Tid old_thread = running_thread;
+        if (ready_queue[0] == (Tid) -300) {return THREAD_NONE;}
+        Tid new_thread_tid = ready_queue[0];
+        new_thread = created_threads[(int)new_thread_tid];
+		
+		running_thread = new_thread ->Tid;
+        remove_from_queue(0);
+        add_to_queue_tail(old_thread);
+		new_thread_id = new_thread->Tid;
+
+		int flag = 0;
+		getcontext(&(curr_thread->context));
+        if (zombie_stack_addr != NULL){
+            free(zombie_stack_addr);
+            zombie_stack_addr = NULL;
+        }
+        if (returning_from_exit == 1){
+            Tid old_thread = running_thread;
+            if (ready_queue[0] == (Tid) -300) {return THREAD_NONE;}
+            Tid new_thread_tid = ready_queue[0];
+            new_thread = created_threads[(int)new_thread_tid];
+		
+		    running_thread = new_thread ->Tid;
+            remove_from_queue(0);
+            add_to_queue_tail(old_thread);
+		    new_thread_id = new_thread->Tid;
+            returning_from_exit = 0;
+        }
+        if (created_threads[(int) running_thread]->killed == 1){
+            thread_exit(-8);
+        } 
+        if (flag ==0){
+		    flag=1;
+		    setcontext(&(new_thread->context));
+        }
+		return new_thread_id;
+
+	} else if (want_tid < 0) {
+        return THREAD_INVALID;
+    } else{
+		
+        // look for thread with Tid = want_tid in queue, if it is not in the queue retrun THREAD_INVALID
+        if (running_thread == want_tid) {return thread_id();}
+		Tid old_thread = running_thread;
+        int index = find_index_in_queue(want_tid);
+        if (index == -1) {return THREAD_INVALID;}
+        new_thread = created_threads[(int) want_tid];
+
+		running_thread = new_thread ->Tid;
+		new_thread_id = new_thread->Tid;
+        // remove new_thread from ready queue and shift values over and place yielded thread at
+        // the tail of the queue
+        remove_from_queue(index);
+        add_to_queue_tail(old_thread);
+
+		int flag = 0;
+		getcontext(&(curr_thread->context));
+        if (zombie_stack_addr != NULL){
+            free(zombie_stack_addr);
+            zombie_stack_addr = NULL;
+        }
+        if ((returning_from_exit == 1) && (zombie_tid == want_tid)){
+            zombie_tid = (Tid) -300;
+            return THREAD_INVALID;
+        }
+        if (created_threads[running_thread]->killed == 1){
+            thread_exit(-8);
+        }
+        // set context
+        if (flag ==0){
+		    flag=1;
+		    setcontext(&(new_thread->context));
+        }
+    }
+	return new_thread_id;
 
 }
 
@@ -299,97 +302,27 @@ Tid find_tail_of_queue(){
 			return want_tid;
 		}
 	}
+	printf("ERROR IN FINDING TAIL OF QUEUE, CHECK EXIT FUNCTION");
 	return -1;
-}
-
-Tid
-thread_yield(Tid want_tid)
-{
-	int e = interrupts_off();
-	/* ERROR CHECKING*/
-    if (want_tid == THREAD_SELF) {
-		interrupts_set(e);
-        return running_thread;
-    } else if (want_tid == THREAD_ANY){
-        if (ready_queue[0] == (Tid)-300) {
-			interrupts_set(e);
-			return THREAD_NONE;}
-    } else if (want_tid < 0){ 
-		interrupts_set(e);
-        return THREAD_INVALID;
-    } else {
-        if (running_thread ==  want_tid){ 
-			interrupts_set(e);
-			return thread_id();}
-        int index = find_index_in_queue(want_tid);
-        if (index == -1){
-			interrupts_set(e);
-			return THREAD_INVALID;}
-    }
-
-
-    /* FIND THREAD YOU WANT TO YIELD TO*/
-    int new_thread_tid;
-    if (want_tid == THREAD_ANY){
-        new_thread_tid = ready_queue[0];
-        remove_from_queue(0);
-    } else {
-        new_thread_tid = want_tid;
-        int index = find_index_in_queue(want_tid);
-        remove_from_queue(index);
-    }
-
-    /* YIELDING */
-    Tid run_thread = running_thread;
-	if (created_threads[(int) running_thread] -> sleeping != true) {add_to_queue_tail(run_thread);}
-    running_thread = new_thread_tid;
-    bool setcontext_called = false;
-	assert (!interrupts_enabled());
-    getcontext(&(created_threads[(int)run_thread]->context));
-
-    if (setcontext_called){
-        thread_routine_cleanup();
-    } else {
-        setcontext_called = true;
-		assert (!interrupts_enabled());
-		assert (created_threads[(int)new_thread_tid]->sleeping == false);
-        setcontext(&(created_threads[(int)new_thread_tid]->context));
-    }
-	interrupts_set(e);
-    return new_thread_tid;
-}
-
-void
-freeup_leftover_zombies(){
-	int e = interrupts_off();
-	if (ready_queue[0]== (Tid)-300){
-		for (int i = 0; i<THREAD_MAX_THREADS; ++i){
-			if (created_threads[i] != NULL && i != running_thread){
-				thread_create_zombie(i);
-				thread_cleanup_zombie(created_threads[i]->stack_freed);
-				
-			}
-		}
-	}
-	interrupts_set(e);
 }
 
 void
 thread_exit(int exit_code)
 {
-	interrupts_off();
-	cleanup_before_zombifying(running_thread);
-	if (ready_queue[0] == (Tid)-300){
-		freeup_leftover_zombies();
-		exit(0);}
+	if (ready_queue[0] == (Tid)-300){exit(0);}
     else{
-		created_threads[(int)running_thread]->exit = exit_code;
-		--num_threads_created;
-        thread_create_zombie(running_thread);
-		running_thread = ready_queue[0];
-		remove_from_queue(0);
-		returning_from_exit = true;
-		assert (!interrupts_enabled());
+        zombie_tid = running_thread;
+        append_to_available(running_thread);
+        --num_threads_created;
+        zombie_stack_addr = created_threads[(int) running_thread]-> stack_addr;
+        free(created_threads[(int)running_thread]);
+        created_threads[(int)running_thread] = NULL;
+
+        Tid new_context_tid = find_tail_of_queue();
+        int i = find_index_in_queue(new_context_tid);
+        remove_from_queue(i);
+        running_thread = new_context_tid;
+        returning_from_exit = 1;
         setcontext(&(created_threads[(int)running_thread]->context));
     }
 }
@@ -397,33 +330,19 @@ thread_exit(int exit_code)
 Tid
 thread_kill(Tid tid)
 {
-	int e = interrupts_off();
-	int i = find_index_in_queue(tid);
 	// check if thread exists, if not return THREAD_INVALID
-	if (tid == running_thread) {
-		interrupts_set(e);
-		return THREAD_INVALID;
-	} else if (tid < 0 || tid >= THREAD_MAX_THREADS){
-		interrupts_set(e);
-		return THREAD_INVALID;
-	} else if (i == -1 && created_threads[tid]!=NULL && created_threads[tid]->sleeping == false){
-		interrupts_set(e);
+	if (tid <= 0){
 		return THREAD_INVALID;
 	} else if (created_threads[(int) tid] == NULL){
-		interrupts_set(e);
 		return THREAD_INVALID;
-	} else if (tid == zombie_tid || created_threads[(int) tid]->killed){
-		interrupts_set(e);
+	} else if (tid == running_thread){
 		return THREAD_INVALID;
 	} else {
 		struct thread* t = created_threads[(int)tid];
-		t->killed = true;
-		if (t->wq != NULL && t->wq->head != NULL){
-			thread_wakeup(t->wq, false);
-		} 
+		t->killed = 1;
+		return tid;
 	}
-	interrupts_set(e);
-	return tid;
+	
 }
 /**************************************************************************
  * Important: The rest of the code should be implemented in Assignment 2. *
